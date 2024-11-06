a6b890b9b6ee99d5c8e956d6be967ada89f487a7
Andy Huang <andy.huang7@student.tdsb.on.ca>
2024-11-06 13:28:57 -0500

    added files for each of the calculators

---
+++ src/components/CosLaw.vue
@@ -0,0 +1,25 @@
+<script>
+function Coslaw() {
+  cValue.value = Math.sqrt(a * a + b * b - (2 * a * b * Math.cos(C)));
+}
+</script>
+
+<template>
+<form action="/action_page.php">
+        <h1>Cos law</h1>
+        <label>a:</label>
+        <input vue-model="side_a" type = "number"/>
+        <label>b:</label>
+        <input vue-model="text" type = "number"/>
+        <label>C:</label>
+        <input vue-model="text" type = "number"/>
+        <label>c:</label>
+        <input vue-model="text" type = "number" readonly/>
+        <input type="button" value="Calculate"/>
+ </form>
+
+</template>
+
+<style scoped>
+
+</style>
